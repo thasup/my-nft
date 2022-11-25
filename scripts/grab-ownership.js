6285c2478275be2821d61ba20bc31f11484d55d3
thasup <thanachonfirst@hotmail.com>
2022-11-25 15:39:22 +0700

    Grabbing an ownership snapshot for a collection

---
+++ scripts/grab-ownership.js
@@ -0,0 +1,36 @@
+// Setup: npm install alchemy-sdk
+require('dotenv').config();
+const { Network, Alchemy } = require("alchemy-sdk");
+
+const API_KEY = process.env.API_KEY;
+
+const config = {
+  apiKey: API_KEY,
+  network: Network.ETH_GOERLI,
+};
+const alchemy = new Alchemy(config);
+
+const main = async () => {
+  // Check if 0xshah.eth owns a Bored Ape.
+  const nfts = await alchemy.nft.getOwnersForContract(
+    "0xf4910c763ed4e47a585e2d34baa9a4b611ae448c", // NFT contract address
+    {
+      withTokenBalances: true,
+    }
+  );
+  // Print NFTs
+  console.log(nfts);
+};
+
+// Execute the code
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
